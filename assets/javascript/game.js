cea2633742efae6465c87ed8ea9c9b00de59385d
onaseer07 <onaseer07@gmail.com>
2018-05-31 22:35:28 -0500

    Completed html structure and game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,142 @@
-//user starts the game by clicking on the "start" button
-$("button").on("click", function() {
-    $("#start").fadeOut();
-    //Game starts
 
-    //Create timer value holder
-    let timer = 10;
-    //Create reset timer function to reset the value to 30
-    let resetTimer = function() {
-        timer = 30;
-        $("#timer").html(timer);
-    }
-    //Create clock count down interval starts at 30 seconds
-    let startInterval = setInterval(function(){
-            $("#timer").html(timer--);
-        },1000)
-    //Create correctly, incorrectly, and unanswered variables to hold the value
-    let correctAnswers = 0;
-    let incorrectAnswers = 0;
-    let unAnswered = 0;
-
-    //Create quiz object containing questions, answers, correct choices and methods
+$(document).ready(function() {
+    let correctAnswerTally = 0;
+    let incorrectAnswerTally = 0;
+    let unansweredTally = 0;
+    let quizCounter = 0;
+    let timerValue;
+    let timer;
+    
+    
+    
     let Quiz = 
         {
-            question1: "What is my Name?",
-            answer1: ["Osman","Bob","Chris"],
-            correctAnswer1: "Osman",
-            userPick: [],
+            Q: ["What is my name?", "How old am I?", "What is my favorite type of food?"],
+            A: ["Osman","Thirty","Italian"], 
+            C: [["Bob","Joe","Osman"],["Fourty","Fifty","Thirty"],["American", "Mexican", "Italian"]],
+            displayQuestion: function(){
+                //Declare conditions
+                if (quizCounter < this.Q.length) {
+    
+                    timerValue = 10;
+                    timer = setInterval(function(){
+                        if (timerValue === 0) {
+                            clearInterval(timer);
+                            loseTimeoutMessage();
+                        } else if (timerValue > 0) {
+                            $("#timer").html(timerValue--);
+                        }
+                    },1000);
+    
+                    //display the question at index of quizCounter
+                    $(".question").html(this.Q[quizCounter]);
+                    console.log(this.Q[quizCounter]); 
+    
+                    //loop through the nested array of choices(C) to display the values as buttons
+                    for (let i = 0; i < this.C.length; i++) {
+                        $(".choices").append("<button class = choicesButton>"+this.C[quizCounter][i]+"</button><br>");
+                        console.log(this.C[quizCounter][i]);
+                    }
+    
+                    //on click of a choice button the system checks the value selected 
+                        //with the correct A index of counterQuiz
+                    $(".choices").on("click","button",function(){
+                        let selectedAnswer = $(this).text();
+                        //if the selected answer matches with the A index of counterQuiz then
+                        if(selectedAnswer == Quiz.A[quizCounter]) {
+                            //update correct answer tally
+                            
+                            console.log("Total Correct Answers: " + correctAnswerTally);
+                            clearInterval(timer);
+                            //set time out to display function winMessage()
+                            winMessage();
+                            
+                        } else {
+                            console.log("Total incorrect Answers: " + incorrectAnswerTally);
+                            clearInterval(timer);
+                            loseMessage();
+                        };
+                    });
+                } else {
+                    displayResults();
+                    clearInterval(timer);
+                };
+                //if the quizCounter is less than length of Q then 
+            },
         };
-        $("#question").html(Quiz.question1);
-        for (let i = 0; i<Quiz.answer1.length; i++){
-            $("#answer").append("<button>" + Quiz.answer1[i] + "</button><br>");
-        }
-
-
-    //A question is generated and displayed
-
-    //Answer choices are displayed for user to choose
-    //If the clock is at zero and the answerchoice is not made then
-        //correct answer is displayed
-        //unanswered counter gets updated
-    //else if the clock is greater than or equal to zero then
-        //if the correct answer choice is made then
-            //correct answered is displayed with 'You are right!'    
-            //correctanswer counter gets updated
-        //else if the wronganswer choice is made then
-            //correct answered is displayed with 'Unfortunately, You guessed wrong!'
-            //wronganswer counter gets updated
-    //Next question is generated and displayed
-    //Answer choices are displayed for user to choose
-
-
-})
-
-
-
-
-
+    
+    
+    //Create winMessage, loseMessage, and loseTimeoutMessage functions
+        //set each message to display for 3 seconds based on the conditions defined in Quiz.displayQuestion(quizCounter);
+            //update relavent tally and resetting the timerValue to 30 seconds
+            //display next question after 3 seconds
+    // ------------------------------------------------------------------------------------------------------------------- //
+    let winMessage = function(){
+        $("body").html(`<div class = "jumbotron text-center Quiz">
+        <h1 class = title>Trivia Quiz!</h1>`+"Right! The correct answer was: " + Quiz.A[quizCounter]);
+        correctAnswerTally++;
+        quizCounter++;
+        timerValue = 10;
+        setTimeout(nextQuestion,3000);
+    };
+    let loseMessage = function(){
+        $("body").html(`<div class = "jumbotron text-center Quiz">
+        <h1 class = title>Trivia Quiz!</h1>`+"Wrong! The correct answer was:  " + Quiz.A[quizCounter]);
+        incorrectAnswerTally++;
+        quizCounter++;
+        timerValue = 10;
+        setTimeout(nextQuestion,3000);
+        
+    };
+    let loseTimeoutMessage = function(){
+        $("body").html(`<div class = "jumbotron text-center Quiz">
+        <h1 class = title>Trivia Quiz!</h1>`+"Time's Up! The correct answer was:  " + Quiz.A[quizCounter]);
+        unansweredTally++;
+        quizCounter++;
+        timerValue = 10;
+        setTimeout(nextQuestion,3000); 
+    };
+    // ------------------------------------------------------------------------------------------------------------------- //
+    
+    //Create nextQuestion function to setup html structure and initialize next question function
+    let nextQuestion = function(){
+        $("body").html(`<div class = "jumbotron text-center">
+            <div class = "Quiz">
+            <h1 class = "title">Trivia Quiz!</h1>
+            <p>Time Remaining: <span id = "timer">10</span></p>
+            <p class = "question"></p>
+            <p class = "choices"></p>
+            <p class = "answer"></p>
+            <p class = "result"></p>
+        </div>
+    </div>`)
+        Quiz.displayQuestion(quizCounter);
+    };
+    
+    //Create displayResults function to show total score and button to restart the game
+    let displayResults = function() {
+        $("body").html(`<div class = "jumbotron text-center Quiz">
+                    <h1 class = title>Trivia Quiz!</h1>`+"<div class = results><h2>You scored: "+parseInt(((Quiz.Q.length-(incorrectAnswerTally+unansweredTally))/Quiz.Q.length)*100)+"%</h2></div>"+"<div class = results> Correct Answers: " + correctAnswerTally + "</div>" + "<div class = results> Incorrect Answers: " + incorrectAnswerTally + "</div>" + "<div class = results> Unanswered: " + unansweredTally + "</div>"+"<div class = reset><button id = resetButton>Start Over?</button>");
+    };
+    //Create a function to setup initial quiz screen html and show button that will start the quiz
+    function initialScreen() {
+        $("body").html(`<div class = "jumbotron text-center"><div class = "Quiz">
+        <h1 class = title>Trivia Quiz!</h1><br><button id = startButton>Play</button></div>`)
+        $("#startButton").on("click", function(){
+        $("body").html(`<div class = "jumbotron text-center">
+            <div class = "Quiz">
+            <h1 class = "title">Trivia Quiz!</h1>
+            <p>Time Remaining: <span id = "timer">10</span></p>
+            <p class = "question"></p>
+            <p class = "choices"></p>
+            <p class = "answer"></p>
+            <p class = "result"></p>
+        </div>
+    </div>`)
+        Quiz.displayQuestion(quizCounter);
+        })
+    };    
+    initialScreen(); 
+    
+    
+});
---
+++ assets/javascript/test.js
@@ -0,0 +1,135 @@
+
+let correctAnswerTally = 0;
+let incorrectAnswerTally = 0;
+let unansweredTally = 0;
+let quizCounter = 0;
+let timerValue;
+let timer;
+
+
+
+let Quiz = 
+    {
+        Q: ["What is my name?", "How old am I?", "What is my favorite type of food?"],
+        A: ["Osman","Thirty","Italian"], 
+        C: [["Bob","Joe","Osman"],["Fourty","Fifty","Thirty"],["American", "Mexican", "Italian"]],
+        displayQuestion: function(){
+            //Declare conditions
+            if (quizCounter < this.Q.length) {
+
+                timerValue = 10;
+                timer = setInterval(function(){
+                    if (timerValue === 0) {
+                        clearInterval(timer);
+                        loseTimeoutMessage();
+                    } else if (timerValue > 0) {
+                        $("#timer").html(timerValue--);
+                    }
+                },1000);
+
+                //display the question at index of quizCounter
+                $(".question").html(this.Q[quizCounter]);
+                console.log(this.Q[quizCounter]); 
+
+                //loop through the nested array of choices(C) to display the values as buttons
+                for (let i = 0; i < this.C.length; i++) {
+                    $(".choices").append("<button>"+this.C[quizCounter][i]+"</button><br>");
+                    console.log(this.C[quizCounter][i]);
+                }
+
+                //on click of a choice button the system checks the value selected 
+                    //with the correct A index of counterQuiz
+                $(".choices").on("click","button",function(){
+                    let selectedAnswer = $(this).text();
+                    //if the selected answer matches with the A index of counterQuiz then
+                    if(selectedAnswer == Quiz.A[quizCounter]) {
+                        //update correct answer tally
+                        
+                        console.log("Total Correct Answers: " + correctAnswerTally);
+                        clearInterval(timer);
+                        //set time out to display function winMessage()
+                        winMessage();
+                        
+                    } else {
+                        console.log("Total incorrect Answers: " + incorrectAnswerTally);
+                        clearInterval(timer);
+                        loseMessage();
+                    };
+                });
+            } else {
+                displayResults();
+                clearInterval(timer);
+            };
+            //if the quizCounter is less than length of Q then 
+        },
+    };
+
+
+//Create winMessage, loseMessage, and loseTimeoutMessage functions
+    //set each message to display for 3 seconds based on the conditions defined in Quiz.displayQuestion(quizCounter);
+        //update relavent tally and resetting the timerValue to 30 seconds
+        //display next question after 3 seconds
+// ------------------------------------------------------------------------------------------------------------------- //
+let winMessage = function(){
+    $("body").html(`<div class = Quiz>
+    <h1 class = title>Trivia Quiz!</h1>`+"Right! The correct answer was: " + Quiz.A[quizCounter]);
+    correctAnswerTally++;
+    quizCounter++;
+    timerValue = 10;
+    setTimeout(nextQuestion,3000);
+};
+let loseMessage = function(){
+    $("body").html(`<div class = Quiz>
+    <h1 class = title>Trivia Quiz!</h1>`+"Wrong! The correct answer was:  " + Quiz.A[quizCounter]);
+    incorrectAnswerTally++;
+    quizCounter++;
+    timerValue = 10;
+    setTimeout(nextQuestion,3000);
+    
+};
+let loseTimeoutMessage = function(){
+    $("body").html(`<div class = Quiz>
+    <h1 class = title>Trivia Quiz!</h1>`+"Time's Up! The correct answer was:  " + Quiz.A[quizCounter]);
+    unansweredTally++;
+    quizCounter++;
+    timerValue = 10;
+    setTimeout(nextQuestion,3000); 
+};
+// ------------------------------------------------------------------------------------------------------------------- //
+
+//Create nextQuestion function to setup html structure and initialize next question function
+let nextQuestion = function(){
+    $("body").html(`<div class = "Quiz">
+    <h1 class = "title">Trivia Quiz!</h1>
+    <p>Time Remaining: <span id = "timer">10</span></p>
+    <p class = "question"></p>
+    <p class = "choices"></p>
+    <p class = "answer"></p>
+    <p class = "result"></p>
+  </div>`)
+    Quiz.displayQuestion(quizCounter);
+};
+
+//Create displayResults function to show total score and button to restart the game
+let displayResults = function() {
+    $("body").html(`<div class = Quiz>
+                <h1 class = title>Trivia Quiz!</h1>`+"<div class = results><h2>You scored: "+parseInt(((Quiz.Q.length-(incorrectAnswerTally+unansweredTally))/Quiz.Q.length)*100)+"%</h2></div>"+"<div class = results> Correct Answers: " + correctAnswerTally + "</div>" + "<div class = results> Incorrect Answers: " + incorrectAnswerTally + "</div>" + "<div class = results> Unanswered: " + unansweredTally + "</div>"+"<div class = reset><button>Start Over?</button>");
+};
+//Create a function to setup initial quiz screen html and show button that will start the quiz
+function initialScreen() {
+    $("body").html(`<div class = Quiz>
+    <h1 class = title>Trivia Quiz!</h1><br><button id = startButton>Play</button>`)
+    $("#startButton").on("click", function(){
+    $("body").html(`<div class = "Quiz">
+    <h1 class = "title">Trivia Quiz!</h1>
+    <p>Time Remaining: <span id = "timer">10</span></p>
+    <p class = "question"></p>
+    <p class = "choices"></p>
+    <p class = "answer"></p>
+    <p class = "result"></p>
+  </div>`)
+    Quiz.displayQuestion(quizCounter);
+    })
+};    
+initialScreen(); 
+
